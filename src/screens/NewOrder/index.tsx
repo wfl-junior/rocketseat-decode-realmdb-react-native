8b45b51e6543ef9603418a76c7af89299ff04603
Wallace Júnior <wflj1997@hotmail.com>
2022-07-27 14:00:05 -0300

    new order screen

--- src/screens/NewOrder/index.tsx
+++ src/screens/NewOrder/index.tsx
@@ -1,23 +1,57 @@
-import React, { useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
-
-import { Container, Header, Title, Form } from './styles';
-
-import { Input } from '../../components/Input';
-import { Button } from '../../components/Button';
-import { TextArea } from '../../components/TextArea';
-import { IconButton } from '../../components/IconButton';
+import { useNavigation } from "@react-navigation/native";
+import React, { useState } from "react";
+import { Alert } from "react-native";
+import uuid from "react-native-uuid";
+import { Button } from "../../components/Button";
+import { IconButton } from "../../components/IconButton";
+import { Input } from "../../components/Input";
+import { TextArea } from "../../components/TextArea";
+import { getRealm } from "../../database/realm";
+import { SchemaTypes } from "../../database/Schemas";
+import { Container, Form, Header, Title } from "./styles";
 
 export function NewOrder() {
   const [isLoading, setIsLoading] = useState(false);
-  const [patrimony, setPatrimony] = useState('');
-  const [equipment, setEquipment] = useState('');
-  const [description, setDescription] = useState('');
+  const [patrimony, setPatrimony] = useState("");
+  const [equipment, setEquipment] = useState("");
+  const [description, setDescription] = useState("");
 
-  const navigation = useNavigation();
+  const { goBack } = useNavigation();
 
   function handleBack() {
-    navigation.goBack();
+    goBack();
+  }
+
+  async function handleCreateNewOrder() {
+    setIsLoading(true);
+    const realm = await getRealm();
+
+    try {
+      realm.write(() => {
+        const createdOrder = realm.create(SchemaTypes.Order, {
+          _id: uuid.v4().toString(),
+          patrimony,
+          equipment,
+          description,
+          status: "open",
+          created_at: new Date(),
+        });
+
+        console.log(createdOrder);
+      });
+
+      setPatrimony("");
+      setEquipment("");
+      setDescription("");
+
+      Alert.alert("Chamado", "Chamado enviado com sucesso.");
+    } catch (error) {
+      console.warn(error);
+      Alert.alert("Chamado", "Não foi possível enviar chamado.");
+    } finally {
+      setIsLoading(false);
+      realm.close();
+    }
   }
 
   return (
@@ -30,17 +64,20 @@ export function NewOrder() {
       <Form>
         <Input
           placeholder="Número do Patrimônio"
+          value={patrimony}
           onChangeText={setPatrimony}
         />
 
         <Input
           placeholder="Equipamento"
+          value={equipment}
           onChangeText={setEquipment}
         />
 
         <TextArea
           placeholder="Descrição"
           autoCorrect={false}
+          value={description}
           onChangeText={setDescription}
         />
       </Form>
@@ -48,6 +85,7 @@ export function NewOrder() {
       <Button
         title="Enviar chamado"
         isLoading={isLoading}
+        onPress={handleCreateNewOrder}
       />
     </Container>
   );
